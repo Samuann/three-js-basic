e7c7982c8d70f9f33f635916b8c0f8717fdb33d4
Samuel <anita.samuel@web.de>
2021-11-03 15:52:11 +0000

    basic scene and material for threeJS

---
+++ script.js
@@ -0,0 +1,46 @@
+// Create a scene
+const scene = new THREE.Scene();
+const canvas = document.querySelector('.webgl');
+
+// Create a geometry of an aquamarine Cube
+const 
+    boxWidth = 1, 
+    boxHeight = 1,
+    boxDepth = 1;
+
+const cubeGeometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+const material = new THREE.MeshBasicMaterial({
+    color: '#a5ffd6'
+});
+
+// Mesh will ontain the material and the geometry
+const cubeMesh = new THREE.Mesh(cubeGeometry, material);
+
+// add mesh to the scene
+scene.add(cubeMesh);
+
+// Sizes
+const renderSize = {
+    width: 800,
+    height: 600
+};
+
+const aspectRatio = renderSize.width/renderSize.height;
+
+const FIELD_OF_VIEW = 75;
+
+// Camera: Point of view using the PerspectiveCamera
+const camera = new THREE.PerspectiveCamera(FIELD_OF_VIEW, aspectRatio);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas
+});
+
+renderer.setSize(renderSize.width, renderSize.height);
+
+renderer.render(scene, camera);
+
+
