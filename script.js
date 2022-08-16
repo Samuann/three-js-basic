0d00736248901b93e10f3df2e3097165a58324d3
Anita Samuel <69172472+Samuann@users.noreply.github.com>
2022-08-16 19:25:11 +0100

    small changes to code.

--- script.js
+++ script.js
@@ -1,46 +1,37 @@
-// Create a scene
-const scene = new THREE.Scene();
-const canvas = document.querySelector('.webgl');
-
-// Create a geometry of an aquamarine Cube
-const boxWidth = 1; 
-const boxHeight = 1;
-const boxDepth = 1;
-
-const cubeGeometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-
-const material = new THREE.MeshBasicMaterial({
-    color: '#a5ffd6'
-});
-
-// Mesh will contain the material and the geometry
-const cubeMesh = new THREE.Mesh(cubeGeometry, material);
-
-// add mesh to the scene
-scene.add(cubeMesh);
-
 // Sizes
 const renderSize = {
     width: 800,
     height: 600
 };
+const cubeWidth = 1; 
+const cubeHeight = 1;
+const cubeDepth = 1;
 
 const aspectRatio = renderSize.width/renderSize.height;
+const VERTICAL_FIELD_OF_VIEW = 75;
 
-const FIELD_OF_VIEW = 75;
-
-// Camera: Point of view using the PerspectiveCamera
-const camera = new THREE.PerspectiveCamera(FIELD_OF_VIEW, aspectRatio);
-camera.position.z = 3;
-scene.add(camera);
-
-// Renderer
+// Set-up
+const canvas = document.querySelector('.webgl');
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(VERTICAL_FIELD_OF_VIEW, aspectRatio); // Camera: Point of view using the PerspectiveCamera in degrees
 const renderer = new THREE.WebGLRenderer({
     canvas
 });
+// calc how large the scene will look like based on what the camera is looking at.
+renderer.setSize(renderSize.width, renderSize.height);  
 
-renderer.setSize(renderSize.width, renderSize.height);
 
+// Create a cube
+const cubeGeometry = new THREE.BoxGeometry(cubeWidth, cubeHeight, cubeDepth);
+const cubeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xa5ffd6
+});
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cube);
+
+// add and position camera
+scene.add(camera);
+camera.position.z = 4;
+
+// render the scene
 renderer.render(scene, camera);
-
-
